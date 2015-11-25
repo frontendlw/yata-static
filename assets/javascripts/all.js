cdefcf715c31a2c8e1abe9b3e53ad1988dc1e271
Deivid Marques <deividmarques@gmail.com>
2015-11-25 20:28:59 -0200

    Automated commit at 2015-11-25 22:28:58 UTC by middleman-deploy 1.0.0

--- assets/javascripts/all.js
+++ assets/javascripts/all.js
@@ -6,6 +6,14 @@ yata.general = (function() {
     dragDropMenu();
     toggleLabelPopover();
     linkPreventDefault();
+    createTitlePage();
+    confirmNewPage();
+    cancelNewPage();
+    openEditPage();
+    showBoxPages();
+    openModal();
+    bindCloseModal();
+    bindCloseModalEsc();
   }
 
 
@@ -32,12 +40,107 @@ yata.general = (function() {
 
 
   function linkPreventDefault(){
-      $("a").on("click", function(e){
-        if($(this).attr("href") === "" || $(this).attr("href") === "#"){
-          e.preventDefault();
-        }
-      })
-    },
+    $('a').on('click', function(e){
+      if($(this).attr('href') === '' || $(this).attr('href') === '#'){
+        e.preventDefault();
+      }
+    })
+  }
+
+  function createTitlePage(){
+    var $field = $('#title-page');
+
+    $field.on('keyup', function(){
+      $('#url-page').val($(this).val());
+    });
+
+  }
+
+  function clearFields(){
+    $('#url-page, #title-page').val("");
+  }
+
+  function loadingPage(){
+    $('.image-fake').addClass('hide');
+    $('.cssload-loader').addClass('show');
+    setTimeout(function(){
+      $('.cssload-loader').removeClass('show');
+      $('.image-empty').addClass('show');
+    },3000)
+  }
+
+
+
+
+  function confirmNewPage(){
+    $('#confirm-add-page').on('click', function(){
+      $('#add-page').trigger('click');
+      loadingPage();
+      clearFields();
+    });
+  }
+
+  function cancelNewPage(){
+    $('#cancel-add-page').on('click', function(){
+      clearFields();
+    });
+  }
+
+  function openEditPage(){
+    $('.cs-ico-config').on('click', function(e){
+      $('#cs-sidebar-edit-page').click();
+      if($('#cs-sidebar-edit-page').is(':checked')){
+        positionArrow(this);
+      }
+      if($('#cs-sidebar-new-page, #cs-sidebar-new-link').is(':checked')){
+        $('#cs-sidebar-new-page, #cs-sidebar-new-link').removeAttr('checked');
+      }
+    });
+  }
+
+
+  function positionArrow(elem){
+    var posY = $(elem).offset().top - 50;
+    $('.cs-sidebar-new-page .cs-sidebar-arrow').css({ top: posY });
+  }
+
+  function showBoxPages(){
+    $('.cs-sidebar-nav-pages > label').on('click', function(){
+      positionArrow(this);
+      if($('#cs-sidebar-edit-page').is(':checked')){
+        $('#cs-sidebar-edit-page').removeAttr('checked');
+      }
+    })
+  }
+
+  function openModal(){
+    $('[data-module="modal"]').on('click', function(){
+      var target = $(this).attr('data-target');
+      $('html').addClass('cs-open-modal');
+      $('#' + target).addClass('opened');
+    });
+  }
+
+  function bindCloseModal(){
+    $('[data-close="modal"]').on('click', function(){
+      closeModal(this);
+    });
+  }
+
+  function closeModal(el){
+    $('html').removeClass('cs-open-modal');
+    $(el).closest('.cs-modal').removeClass('opened');
+  }
+
+
+  function bindCloseModalEsc() {
+    $(document).on('keyup', function (e) {
+      if(e.keyCode === 27){
+        closeModal('[data-close="modal"]');
+      }
+    });
+
+  }
 
   return {
     init: init
